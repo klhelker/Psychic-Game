54d1360e51b0bcd3dd343ab2637bf01c0db6aee6
Kristin Helker <klhelker@gmail.com>
2020-01-06 14:55:26 -0600

    finished game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,58 +1,51 @@
+var computerChoices = ["a","b","c","d","e","f","g","h","i","j","i","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-  
-    var computerChoices = ["a","b","c","d","e","f","g","h","i","j","i","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-    
-    var wins = 0;
-    var losses = 0;
-    var guessesLeft= 5;
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-    var guessesSoFar = []
-    
-    document.onkeyup = function(event) {
-        
-      console.log (guessesLeft);
+var wins = 0;
+var losses = 0;
+var guessesLeft= 5;
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+var guessesSoFar = []
 
-      var userChoiceText = document.getElementById("userchoice-text");
-      var computerChoiceText = document.getElementById("computerchoice-text");
-      var winsText = document.getElementById("wins-text");
-      var lossesText = document.getElementById("losses-text");  
-      var guessesText = document.getElementById("guessesLeft-text");
-      
-      var userGuess = event.key;
-          guessesSoFar.push(userGuess);
-          console.log (guessesSoFar);
-          guessesLeft -=1;
-          
-  
-          if (userGuess === computerGuess) 
+document.onkeyup = function(event) {
 
-            { 
-              wins++;
-          
-          } else if (guessesLeft === 0)
-             { 
-               losses++;
-               alert("To play again, hit any button")
-               guessesSoFar = [];
-               guessesLeft=5;
-               
-          }
+  console.log (guessesLeft);
 
-          computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-               
-              
-            
-              console.log(guessesLeft);
+  var userChoiceText = document.getElementById("userchoice-text");
+  var computerChoiceText = document.getElementById("computerchoice-text");
+  var winsText = document.getElementById("wins-text");
+  var lossesText = document.getElementById("losses-text");
+  var guessesText = document.getElementById("guessesLeft-text");
+  var guessesSoFarText = document.getElementById("guessesSoFar-text");
 
-            
-        
-              
-        userChoiceText.textContent = "You picked " + userGuess + ;
-        computerChoiceText.textContent = "Computer picked " + computerGuess;
-        winsText.textContent = "You Won! " + wins;
-        lossesText.textContent =  "You Lost! " + losses;
-        guessesText.textContent =  "You have " + guessesLeft + " chances left!";
-        guessesSoFar.textContent = "your guesses so far " + userGuess;
-      }
-      
+  var userGuess = event.key;
+  guessesSoFar.push(userGuess);
+  document.getElementById(userGuess);
+  guessesLeft -=1;
+
+  if (userGuess === computerGuess)
+  {
+    wins++;
+    guessesLeft=0;
+  } else if(guessesLeft === 0 && userGuess !== computerGuess) {
+    losses++;
+  }
+
+  if (guessesLeft === 0) {
+    userChoiceText.textContent = "You picked " + userGuess;
+    computerChoiceText.textContent = "Computer picked " + computerGuess;
+    guessesSoFar = [];
+    guessesLeft=5;
+    userGuess='';
+    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    alert("To play again, hit any button")
+  } else {
+    computerChoiceText.textContent = "Computer picked " + "";
+    userChoiceText.textContent = "You picked " + userGuess;
+  }
+
+    winsText.textContent = "You Won! " + wins;
+    lossesText.textContent =  "You Lost! " + losses;
+    guessesText.textContent =  "You have " + guessesLeft + " chances left!";
+    guessesSoFarText.textContent = "your guesses so far " + guessesSoFar;
+}
